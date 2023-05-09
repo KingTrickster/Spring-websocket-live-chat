8887a4e021f5650729086d3d520ae626d3f38010
Alejandro Trejo <alextrejores@gmail.com>
2023-05-08 19:16:29 -0600

    first commit

---
+++ src/main/java/com/javatechie/spring/ws/api/model/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.javatechie.spring.ws.api.model;
+
+import lombok.Data;
+
+@Data
+public class ChatMessage {
+	private String content;
+	private String sender;
+	private MessageType type;
+
+	public enum MessageType {
+		CHAT, LEAVE, JOIN
+	}
+
+}
